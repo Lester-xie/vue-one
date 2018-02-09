885231afd1320a24c166ff3090c63c62ba278df7
谢显东 <541898607@qq.com>
2018-02-09 11:24:07 +0800

    🎉 Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+  @import './style/normalize.css';
+  @import './style/common.scss';
+</style>
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+const API = {
+  getList: 'http://v3.wufazhuce.com:8000/api/channel/one/0/Guangzhou?platform=ios',
+};
+
+export default API;
