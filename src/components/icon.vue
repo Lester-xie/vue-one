4b7017b336c0e7a05eebb3f49cfbb7dabd37047e
谢显东 <541898607@qq.com>
2018-03-05 12:15:10 +0800

    补全图标组件

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <img src="../assets/img/like.png" v-if="type === 'like'">
+    <img src="../assets/img/like-active.png" v-if="type === 'likeActive'">
     <img src="../assets/img/discover.png" v-else-if="type === 'discover'">
     <img src="../assets/img/bookmark.png" v-else-if="type === 'bookmark'">
     <img src="../assets/img/pencil.png" v-else-if="type === 'pencil'">
     <img src="../assets/img/share.png" v-else-if="type === 'share'">
+    <img src="../assets/img/arrow-top.png" v-else-if="type === 'arrow-top'">
+    <img src="../assets/img/arrow-bottom.png" v-else-if="type === 'arrow-bottom'">
+    <img src="../assets/img/arrow-right.png" v-else-if="type === 'arrow-right'">
+    <img src="../assets/img/play.png" v-else-if="type === 'play'">
+    <img src="../assets/img/pause.png" v-else-if="type === 'pause'">
+    <img src="../assets/img/player.png" v-else-if="type === 'player'">
   </div>
 </template>
 
